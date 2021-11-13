12bd7719f1a8d3db040ead31841c85bdad934763
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-11-13 13:06:14 +0800

    修改第一個路由：取得所有的任務&呈現

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,16 @@ use Illuminate\Http\Request;
 |
 */
 
+//Route::get('/', function () {
+//    return view('tasks');
+//});
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at','asc')->get();
+    //利用model Task由DB的tasks資料表取出資料並排序
+    //暫存 $tasks
+    //可簡化為 $tasks= Task->get();
+    return view('tasks',['tasks'=>$tasks]);
+    //將取出的資料$tasks傳遞給tasks視圖
 });
 Route::post('/task',function (Request $request){
    //驗證輸入
